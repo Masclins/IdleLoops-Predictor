259205b432aa54c2577d1d4e550ef72324d777ec
Tomnar9 <109023593+Tomnar9@users.noreply.github.com>
2022-10-30 20:07:15 +0100

    version incremented, tooltip changes

--- idleloops-predictor.user.js
+++ idleloops-predictor.user.js
@@ -2,7 +2,7 @@
 // @name         IdleLoops Predictor Makro
 // @namespace    https://github.com/MakroCZ/
 // @downloadURL  https://raw.githubusercontent.com/MakroCZ/IdleLoops-Predictor/master/idleloops-predictor.user.js
-// @version      2.3.8
+// @version      2.3.9
 // @description  Predicts the amount of resources spent and gained by each action in the action list. Valid as of IdleLoops v.85/Omsi6.
 // @author       Koviko <koviko.net@gmail.com>
 // @match        https://lloyd-delacroix.github.io/omsi-loops/
@@ -591,7 +591,7 @@ const Koviko = {
 
       \$('#actionChanges').children('div:nth-child(2)').append("<select id='trackedStat' class='button'></select>");
       \$('#trackedStat').append("<option value=Rsoul hidden=''>(R) Soulstones</option>");
-      \$('#trackedStat').append("<option value=RsoulNow hidden=''>(R) Soulstones Expected</option>");
+      \$('#trackedStat').append("<option value=RsoulNow hidden=''>(R) SS Expected</option>");
       \$('#trackedStat').append("<option value=Ract hidden=''>(R) Final Actions</option>");
       \$('#trackedStat').append("<option value=Rsurvey hidden=''>(R) Surveys</option>");
       \$('#trackedStat').append("<option value=Rinvest hidden=''>(R) Investment</option>");
